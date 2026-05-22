fc568f02a2e1a832a7fd7a63f4ccd88c5786f405
abhay0614 <abhay.singh0614@gmail.com>
2026-05-22 11:20:58 +0530

    recursive top down solution

---
+++ F-LCS.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n,m;
+string s,t;
+
+int rec(int i, int j, vector<vector<int>>& dp)
+{
+	if(i>n-1 || j>m-1) return 0;
+
+	if(dp[i][j] != -1) return dp[i][j];
+
+	if(s[i] == t[j]) 
+    	return dp[i][j] = 1 + rec(i+1, j+1, dp);
+	else
+    	return dp[i][j] = max(rec(i+1, j, dp), rec(i, j+1, dp));
+}
+
+string lcs(int i, int j, vector<vector<int>>& dp)
+{
+	string ans;
+
+	while(i<n && j<m)
+	{
+		if(s[i] == t[j])
+		{
+			ans+=s[i];
+			i++;
+			j++;
+
+			continue;
+		}
+		
+		int move1=dp[i+1][j];
+		int move2=dp[i][j+1];
+
+		if(move1 >= move2) i++;
+		else j++;
+		
+	}
+
+	return ans;
+}
+
+int main()
+{
+	cin >> s >> t;
+	n=s.length(), m=t.length();
+	vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+	if(rec(0,0,dp) > 0) cout << lcs(0,0,dp);
+}
